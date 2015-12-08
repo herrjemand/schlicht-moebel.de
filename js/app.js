80cc5986ea3ba2a9f59df181829d7e5a217c0fb6
Ackermann Yuriy <ackermann.yuriy@gmail.com>
2015-12-08 19:11:50 +1300

    Added resizing. Moved photos to auto filling divs.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 $(document).foundation();
 
-$(window).resize(function() {
-    if ($(window).width() < 769) {
+var resize = function(){
+    if ($(window).width() < 644) {
         $('.off-canvas-wrap').removeClass('move-right');
         $('.left-off-canvas-toggle').show();
+        $('.schlicht-content').css('padding-left', '0');
     } else {
         $('.off-canvas-wrap').addClass('move-right');
         $('.left-off-canvas-toggle').hide();
+        $('.schlicht-content').css('padding-left', '3.5rem');
     }  
+}
+
+$(window).resize(function() {
+    resize();
 });
+
+resize();
